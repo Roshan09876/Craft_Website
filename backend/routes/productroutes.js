6b08f8346c9baef6f2e25b6e80ec2a18fa57b580
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2024-08-14 22:59:31 +0545

    xsrf

--- backend/routes/productroutes.js
+++ backend/routes/productroutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router()
-const productController = require("../controller/productController")
+const productController = require("../controller/productController");
+const { authGuardAdmin } = require("../middleware/authguard");
 
-router.post("/create", productController.createProduct);
+router.post("/create", authGuardAdmin, productController.createProduct);
 router.get("/getallproduct", productController.getallProduct);
-router.delete("/delete/:id", productController.deleteProductById);
-router.put("/update/:id", productController.updateProduct);
+router.delete("/delete/:id",authGuardAdmin, productController.deleteProductById);
+router.put("/update/:id",authGuardAdmin, productController.updateProduct);
 router.get("/product/:id", productController.getProductById);
 
 module.exports = router
